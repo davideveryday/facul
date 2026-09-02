f54f492608e3e31dbc1427d3864a824e219468d9
davideveryday <david.castrosp80@gmail.com>
2026-09-02 11:38:41 -0300

    interface

--- semestre-04/atividade_lista.c
+++ semestre-04/atividade_lista.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+void main(void) {
+	setlocale(LC_ALL, "Portuguese");
+
+    int opcao;
+
+    while(1) {
+		printf("\n1 - Adicionar pessoa\n2 - Remover pessoa");
+		printf("\n3 - Mostrar Fila\n0 - Sair\n\nOpção -> ");
+		scanf("%d", &opcao);
+
+		switch(opcao){
+			case 0: exit(0);
+			case 1:
+                printf("\nOpt 1");
+				break;
+			case 2:
+                printf("\nOpt 2");
+				break;
+			case 3:
+                printf("\nOpt 3");
+				break;
+			default:
+				printf("\n Opção inválida!");	
+		}	
+	}			
 }
