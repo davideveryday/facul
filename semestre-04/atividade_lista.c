8a0d8cc92b5da298e5708d18b81aad6a1baee135
davideveryday <david.castrosp80@gmail.com>
2026-09-04 16:37:36 -0300

    nem lembro mais

--- semestre-04/atividade_lista.c
+++ semestre-04/atividade_lista.c
@@ -23,63 +23,102 @@ void criarFila(Fila *f, unsigned int c) {
 	f->tamanho = 0;
 }
 
-void adicionarPessoa(Fila *f, char *nome) {
-	// ve se a fila tem espaço
-	// se sim ve onde tem espaço
-	// adiciona a pessoa naquele lugar da fila
-	// atualiza o ultimo elemento
-	if (f->ultimo == f->capacidade - 1) f->ultimo = -1;
-	f->ultimo++;
-	strcpy(f->pessoas[f->ultimo], nome);
-	f->tamanho++;
-	
-}
 
-void imprimirFila(Fila *f) {
-	int contador, i;
-
-	for (contador = 0, i = f->primeiro; contador < f->tamanho; contador++) {
-		printf("%s\t", f->pessoas[i++]);
-		if (i == f->capacidade) i=0;
-	}
-
-}
+int taVazia(Fila *f) {
+	return (f->tamanho == 0);
+}	
 
 int taCheia(Fila *f) {
 	return (f->tamanho == f->capacidade);
 }
 
-// v1 -> lista fixa 
-void main(void) {
+void adicionarPessoa(Fila *f) {
+	if (taCheia(f)) {
+		printf("\nFila cheia!\n");
+		return;
+	}
+
+	char nome[TAM_NOME];
+
+	printf("\n--> ");
+
+	if (fgets(nome, TAM_NOME, stdin) != NULL) {
+		// remove a quebra de linha
+		nome[strcspn(nome, "\n")] = 0;
+	}
+
+	if (f->ultimo == f->capacidade - 1) f->ultimo = -1;
+	f->ultimo++;
+	strcpy(f->pessoas[f->ultimo], nome);
+	f->tamanho++;
+}
+
+char* removerPessoa(Fila *f) {
+	if (!taVazia(f)) {
+		char *temp = f->pessoas[f->primeiro++];
+		if (f->primeiro == f->capacidade)
+			f->primeiro = 0;
+		f->tamanho--;
+		
+		return temp;
+	}
+
+	return NULL;
+}
+
+void imprimirFila(Fila *f) {
+	int contador, i;
+
+	printf("\n");
+	for (contador = 0, i = f->primeiro; contador < f->tamanho; contador++) {
+		printf("%s -> ", f->pessoas[i++]);
+		if (i == f->capacidade) i = 0;
+	}
+	printf("\n");
+}
+
+void imprimirTamanhoFila(Fila *f) {
+	printf("\nA fila possui %d pessoas\n", f->tamanho);
+}
+
+int main(void) {
 	setlocale(LC_ALL, "Portuguese");
 
     int opcao;
-    char *nome;
+	char *removido;
     Fila f;
 
     criarFila(&f, MAX);
 
     while(1) {
 		printf("\n1 - Adicionar pessoa\n2 - Remover pessoa");
-		printf("\n3 - Mostrar Fila\n0 - Sair\n\nOpção -> ");
+		printf("\n3 - Mostrar Fila\n4 - Mostrar tamanho da fila\n5 - Buscar por nome\n0 - Sair\n\nOpção -> ");
         scanf("%d", &opcao);
-        while (getchar() != '\n');
+		
+		// limpa quebra de linha
+        while (getchar() != '\n') {};
 
 		switch(opcao){
-			case 0: exit(0);
+			case 0: 
+			 	free(f.pessoas);
+				exit(0);
 			case 1:
-                printf("\n--> ");
-                fgets(nome, TAM_NOME, stdin);
-				adicionarPessoa(&f, nome);
+				adicionarPessoa(&f);
 				break;
 			case 2:
-                printf("\nOpt 2");
+				if ((removido = removerPessoa(&f)))
+					printf("\n%s foi removido da lista.\n", removido);
 				break;
 			case 3:
 				imprimirFila(&f);
 				break;
+			case 4:
+				imprimirTamanhoFila(&f);				// remove a quebra de linha
+				break;
 			default:
 				printf("\n Opção inválida!");	
-		}	
-	}			
+		}
+	}
+	
+	return 0;
 }
